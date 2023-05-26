5783d366e8d9d21bc79622599c74a64d4c4e4cc5
Wessley <wessleynyakaz@gmail.com>
2023-05-26 21:08:03 +0200

    Added gui entry point

--- main.py
+++ main.py
@@ -3,4 +3,17 @@ Defines the UI and link user, os data to instructions.
 Starts, monitor the program.
 Decide to bring on the ui or go background
 '''
-import 
+from customtkinter import CTk
+
+class Main(CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("300x300")
+        self.title('A.D.E.N')
+        self.resizable(False, False)
+
+
+
+if __name__ == '__main__':
+    system = Main()
+    system.mainloop()
