4cb4a2530e33790ed2a2103b9652bdda10a4aca3
Wessley <wessleynyakaz@gmail.com>
2023-06-02 10:03:42 +0200

    ..

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ except: from .src.app import Main
 finally:
     import sys
     if __name__ == '__main__':
-        if getattr(sys, 'frozen', False):import pyi_splash
+        if getattr(sys, 'frozen', False): import pyi_splash
         system = Main()
         # Close the wecome after loading the app
-        if getattr(sys, 'frozen', False):pyi_splash.close()
+        if getattr(sys, 'frozen', False): pyi_splash.close()
         system.mainloop()
